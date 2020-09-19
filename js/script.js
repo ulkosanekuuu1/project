8d1902a8f35f530713edefb74b09a87ed9e9ef8e
sasha <ulkosanekuuu@yandex.ru>
2020-09-19 19:45:50 +0300

    first js api

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
  'use strict';
- const name1 = prompt("kak tebya zovut", '');
- alert(`privet drug po imeni ${name1}`);
+ const numberOfFilms = prompt("how long u watch films?", "");
+ const personalMovieDB = {
+     count: numberOfFilms,
+     movies: {},
+     actors: {},
+     geners: [],
+     privat: false
+ };
+
+ personalMovieDB.movies[prompt("last film")] = prompt("оцените его");
+ console.log(personalMovieDB);
