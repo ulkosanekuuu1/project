487baf608ba9a587e2ad18cb5b5fbb6cf16b68fe
sasha <ulkosanekuuu@yandex.ru>
2020-09-20 16:23:06 +0300

    циклы

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
- 'use strict';
- const numberOfFilms = prompt("how long u watch films?", "");
- const personalMovieDB = {
-     count: numberOfFilms,
-     movies: {},
-     actors: {},
-     geners: [],
-     privat: false
- };
+"use strict";
+const numberOfFilms = prompt("how long u watch films?", "");
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  geners: [],
+  privat: false,
+};
+for (let i = 0; i < personalMovieDB.count; i++) {
+  let nameFIlm = "";
 
- personalMovieDB.movies[prompt("last film")] = prompt("оцените его");
- console.log(personalMovieDB);
+  while (nameFIlm == null || nameFIlm.length > 50 || nameFIlm == "") {
+    nameFIlm = prompt("vvedite film");
+  }
+
+  personalMovieDB.movies[nameFIlm] = prompt("write stars of fiml");
+}
+console.log(personalMovieDB);
+personalMovieDB.count < 2
+  ? alert("любитель")
+  : personalMovieDB.count < 4
+  ? alert("средний")
+  : alert("профант");
