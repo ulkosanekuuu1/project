764cd13438b3c526ae045bc7c79c75b9c5edcd5c
sasha <ulkosanekuuu@yandex.ru>
2020-09-21 12:05:55 +0300

    functions

--- js/script.js
+++ js/script.js
@@ -7,18 +7,34 @@ const personalMovieDB = {
   geners: [],
   privat: false,
 };
-for (let i = 0; i < personalMovieDB.count; i++) {
-  let nameFIlm = "";
 
-  while (nameFIlm == null || nameFIlm.length > 50 || nameFIlm == "") {
-    nameFIlm = prompt("vvedite film");
+function watchedfilm() {
+  for (let i = 0; i < personalMovieDB.count; i++) {
+    let nameFIlm = "";
+
+    while (nameFIlm == null || nameFIlm.length > 50 || nameFIlm == "") {
+      nameFIlm = prompt("vvedite film");
+    }
+
+    personalMovieDB.movies[nameFIlm] = prompt("write stars of fiml");
+  }
+  console.log(personalMovieDB);
+  personalMovieDB.count < 2 ? alert("любитель") : personalMovieDB.count < 4 ? alert("средний") : alert("профант");
+}
+
+function showMyDB() {
+
+  if (!personalMovieDB.privat) {
+    console.log(personalMovieDB)
   }
 
-  personalMovieDB.movies[nameFIlm] = prompt("write stars of fiml");
 }
-console.log(personalMovieDB);
-personalMovieDB.count < 2
-  ? alert("любитель")
-  : personalMovieDB.count < 4
-  ? alert("средний")
-  : alert("профант");
+
+function writeYourGenres() {
+
+  for (let i = 0; i < 3; i++) {
+    personalMovieDB.geners[i] = prompt(`ваш любимы жанр под номером ${i+1}`, '');
+  }
+
+}
+
