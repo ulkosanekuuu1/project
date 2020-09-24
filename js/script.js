8e9f0961e706cae713435df2bb06fcae2caf3fc7
sasha <ulkosanekuuu@yandex.ru>
2020-09-24 10:51:23 +0300

    traning object

--- js/script.js
+++ js/script.js
@@ -1,40 +1,48 @@
 "use strict";
-const numberOfFilms = prompt("how long u watch films?", "");
+
 const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  geners: [],
-  privat: false,
+    //   count: prompt("how long u watch films?", ""),
+    movies: {},
+    actors: {},
+    geners: [],
+    private: true,
+
+    watchedfilm: function () {
+        for (let i = 0; i < personalMovieDB.count; i++) {
+            let nameFIlm = "";
+            while (nameFIlm == null || nameFIlm.length > 50 || nameFIlm == "") {
+                nameFIlm = prompt("vvedite film");
+            }
+            personalMovieDB.movies[nameFIlm] = prompt("write stars of fiml");
+        }
+    },
+
+    showMyDB: function () {
+        if (!personalMovieDB.private) {
+            console.log(personalMovieDB);
+        }
+    },
+
+    writeYourGenres: function () {
+        const howGeners = +prompt('сколько у вас любимых жанпрв?');
+        let geners = '';
+
+        for (let i = 0; i < howGeners; i++) {
+            while (geners == "" || geners == null) {
+                geners = prompt("введите один из любимых жанров");
+            }
+            personalMovieDB.geners[i] = geners;
+            geners = "";
+        }
+    },
+
+    toggleVisibleMyDB: function () {
+        this.private ? (this.private = false) : (this.private = true);
+    },
 };
 
-function watchedfilm() {
-  for (let i = 0; i < personalMovieDB.count; i++) {
-    let nameFIlm = "";
 
-    while (nameFIlm == null || nameFIlm.length > 50 || nameFIlm == "") {
-      nameFIlm = prompt("vvedite film");
-    }
-
-    personalMovieDB.movies[nameFIlm] = prompt("write stars of fiml");
-  }
-  console.log(personalMovieDB);
-  personalMovieDB.count < 2 ? alert("любитель") : personalMovieDB.count < 4 ? alert("средний") : alert("профант");
-}
-
-function showMyDB() {
-
-  if (!personalMovieDB.privat) {
-    console.log(personalMovieDB)
-  }
-
-}
-
-function writeYourGenres() {
-
-  for (let i = 0; i < 3; i++) {
-    personalMovieDB.geners[i] = prompt(`ваш любимы жанр под номером ${i+1}`, '');
-  }
-
-}
-
+personalMovieDB.writeYourGenres();
+personalMovieDB.geners.forEach(function (item, i) {
+    console.log(`любимый жанр #${i+1} это ${item}`);
+});
